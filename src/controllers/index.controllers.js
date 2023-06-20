c7ea2125f4fa2c2051d53c19f948b5c0986bce2b
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2023-06-20 17:36:48 -0500

    Add files via upload

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,8 +1,10 @@
 
-const renderIndex = (req,res)=>{
-    res.render('index')
-}
+const Portfolio = require('../models/Portfolio')
 
+const renderIndex = async(req,res)=>{
+    const portfolios = await Portfolio.find().lean()
+    res.render('index',{portfolios})
+}
 const renderAbout = (req,res)=>{
     res.render('login')
 }
